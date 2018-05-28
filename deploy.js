f75056ad6305e4af32e453a3d26155a1832462eb
spoperechnyi <poperechnyi1@gmail.com>
2018-05-28 22:16:53 +0300

    add deploy part

--- deploy.js
+++ deploy.js
@@ -9,3 +9,24 @@ let privateKey = '*****';
 web3.eth.accounts.wallet.add("0x"+privateKey);
 
 var contractDeploy = new web3.eth.Contract(swapABI);
+
+contractDeploy.deploy({
+    data: swapByteCode,
+    arguments:[]
+}).send({
+    from: publicKey,
+    gas: 30000000,
+    gasPrice: '210000000'
+}, function (error, transactionHash) {
+}).on('error', function (error) {
+    console.log('error', error);
+}).on('transactionHash', function (transactionHash) {
+    console.log('transactionHash', transactionHash);
+}).on('receipt', function (receipt) {
+    console.log('receipt', receipt.contractAddress);
+}).on('confirmation', function (confirmationNumber, receipt) {
+    console.log('confirmation', confirmationNumber);
+}).then(function (new_contract) {
+    console.log('Contract Deployed');
+    console.log(new_contract.options.address);
+});
