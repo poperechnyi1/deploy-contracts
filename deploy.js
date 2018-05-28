01f77f4a43774c63637607d20e5c5ab17dd4a974
spoperechnyi <poperechnyi1@gmail.com>
2018-05-28 22:15:49 +0300

    add deploy script

---
+++ deploy.js
@@ -0,0 +1,7 @@
+var fs = require('fs');
+var Web3 = require('web3');
+
+var web3 = new Web3(new Web3.providers.HttpProvider('****'));
+
+let publicKey = '0x372967130b1A7cd386348Bc59de905e628E36F3a';
+let privateKey = '*****';
