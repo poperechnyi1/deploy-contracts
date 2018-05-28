19fbc593682f76b07894cc630de77bf75d0cd989
spoperechnyi <poperechnyi1@gmail.com>
2018-05-28 22:16:21 +0300

    unlock account

--- deploy.js
+++ deploy.js
@@ -5,3 +5,7 @@ var web3 = new Web3(new Web3.providers.HttpProvider('****'));
 
 let publicKey = '0x372967130b1A7cd386348Bc59de905e628E36F3a';
 let privateKey = '*****';
+
+web3.eth.accounts.wallet.add("0x"+privateKey);
+
+var contractDeploy = new web3.eth.Contract(swapABI);
